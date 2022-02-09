f1de5c3b22c3869a9b7bb03647a03b46f0ffda8c
Rama Deshpande <rama@deshpandez.com>
2022-02-08 23:46:31 -0500

    commit
    commit

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "JSYlgbZTIpkxljt0K2EM0X4Q4", consumerSecret: "wV5XAUT12ISWskjT8O52GF7bNOYYRFVYKzmgXTmSatavyA0X1Y")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
